2b99bf11ac07f5ff226c5f93e37bbe1997d775b9
Forhan96 <f.a.chowdhury19@gmail.com>
2022-09-29 16:44:42 +0600

    - Image full screen

--- lib/presentation/common/router/router_helper.dart
+++ lib/presentation/common/router/router_helper.dart
@@ -6,7 +6,6 @@ class RouterHelper {
   static final FluroRouter router = FluroRouter();
 
   static final Handler _splashScreenHandler = Handler(handlerFunc: (context, parameters) => SplashScreen());
-  // static final Handler _detailScreenHandler = Handler(handlerFunc: (context, parameters) => DetailScreen(index: parameters['index']?[0] ?? ""));
 
   static final Handler _notFoundHandler = Handler(handlerFunc: (context, parameters) {
     print("No Router Found!");
@@ -18,11 +17,6 @@ class RouterHelper {
       handler: _splashScreenHandler,
       transitionType: TransitionType.fadeIn,
     );
-    // router.define(
-    //   Routes.DETAIL_SCREEN,
-    //   handler: _detailScreenHandler,
-    //   transitionType: TransitionType.fadeIn,
-    // );
 
     router.notFoundHandler = _notFoundHandler;
   }
